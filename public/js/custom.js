9fb40d0a390edde15db8e6340c81589653225e59
dohun1109 <dohun1109@naver.com>
2023-06-11 17:57:33 +0900

    final final result

--- public/js/custom.js
+++ public/js/custom.js
@@ -120,12 +120,13 @@ function getURL() { window.location.href; } var protocol = location.protocol; $.
         $(".collapse.show").each(function(){
           $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
         });
-        
+
         // Toggle plus minus icon on show hide of collapse element
         $(".collapse").on('show.bs.collapse', function(){
           $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
         }).on('hide.bs.collapse', function(){
           $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
         });
+
     });
 
